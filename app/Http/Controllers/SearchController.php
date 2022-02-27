378f8ca5e7485595529ed6dd13194008da856df4
Aaron Branson Li <bransonchen1014@gmail.com>
2022-02-27 14:36:36 +0800

    fix search function

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -27,19 +27,18 @@ class SearchController extends Controller
         $related_notes = array();
         $related_resources = array();
 
-
         //search for all the related 
         for ($x = 0; $x < count($words); $x++) {
             for ($y = 0; $y < count($discussions); $y++) {
                 $discussion_name = $discussions[$y]->name;
-                if (strstr($discussion_name, $words[$x])){
+                if (strstr($discussion_name, $words[$x]) !== false){
                     array_push($related_discussions, $discussions[$x]);
                 }
             }
 
             for ($y = 0; $y < count($replies); $y++) {
                 $reply_content = $replies[$y]->content;
-                if (strstr($reply_content, $words[$x])){
+                if (strstr($reply_content, $words[$x]) !== false){
                     $discussion = Discussion::where("id", $replies[$x]->discussion_id)->first();
                     array_push($related_discussions, $discussion);
                 }
@@ -49,9 +48,9 @@ class SearchController extends Controller
             for ($y = 0; $y < count($notes); $y++) {
                 $note_name = $notes[$y]->name;
                 $note_description = $notes[$y]->description;
-                if (strstr($note_name, $words[$x])){
+                if (strstr($note_name, $words[$x]) !== false){
                     array_push($related_notes, $notes[$x]);
-                } elseif (strstr($note_description, $words[$x])) {
+                } elseif (strstr($note_description, $words[$x]) !== false) {
                     array_push($related_notes, $notes[$x]);
                 }
             }
@@ -59,9 +58,9 @@ class SearchController extends Controller
             for ($y = 0; $y < count($resources); $y++) {
                 $resource_name = $resources[$y]->name;
                 $resource_description = $resources[$y]->description;
-                if (strstr($resource_name, $words[$x])){
+                if (strstr($resource_name, $words[$x]) !== false){
                     array_push($related_resources, $resources[$x]);
-                } elseif (strstr($resource_description, $words[$x])){
+                } elseif (strstr($resource_description, $words[$x]) !== false){
                     array_push($related_resources, $resources[$x]);
                 }
             }
